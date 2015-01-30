d3f5ca2967c58d86512df8c15e47605d1fed0e80
Charles Hansen and Ryan Dy <pair+chansen+rdy@pivotal.io>
2015-01-30 14:29:49 -0800

    Add spec swapping script for webstorm

---
+++ scripts/open_spec_or_impl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "This script must be called with an argument" >&2
+    exit 1
+fi
+
+
+echo $1
+if [[ "$1" =~ .*spec\.js ]]; then
+    #in a spec file
+    file_to_open_part1=${1/spec\//\/}
+    file_to_open=${file_to_open_part1/_spec\.js/\.js}
+else
+    file_to_open_part1=${1/app\//spec\/app\/}
+    file_to_open=${file_to_open_part1/\.js/_spec\.js}
+fi
+
+wstorm $file_to_open
+
