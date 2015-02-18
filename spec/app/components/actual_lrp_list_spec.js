afac76c7390c6dc83f31d264091c3f652e97c064
Charles Hansen and Ryan Dy <pair+chansen+rdy@pivotal.io>
2015-02-18 14:56:55 -0800

    Crashed ActualLRPs in Detail Mode render in red [finishes #87227562]

--- spec/app/components/actual_lrp_list_spec.js
+++ spec/app/components/actual_lrp_list_spec.js
@@ -4,7 +4,12 @@ describe('ActualLrpList', function() {
   var actualLrps, subject;
   beforeEach(function() {
     var ActualLrpList = require('../../../app/components/actual_lrp_list');
-    actualLrps = Factory.buildList('actualLrp', 3);
+    actualLrps = [
+      Factory.build('actualLrp', {index: 0}),
+      Factory.build('actualLrp', {index: 1}),
+      Factory.build('actualLrp', {index: 2, state: 'CRASHED'})
+    ]
+    ;
     subject = React.render(<ActualLrpList {...{actualLrps}}/>, root);
   });
 
@@ -14,5 +19,10 @@ describe('ActualLrpList', function() {
 
   it('renders actual lrps', function() {
     expect('.actual-lrp').toHaveLength(actualLrps.length);
+  });
+
+  it('renders crashed lrps differently', function() {
+    expect('.actual-lrp:eq(0)').not.toHaveClass('bg-error-1');
+    expect('.actual-lrp:eq(2)').toHaveClass('bg-error-1');
   });
 });
