89824840e4b759a6cdb523578f90c0dee53ddbc2
Charles Hansen and Ryan Dy <pair+chansen+rdy@pivotal.io>
2015-02-18 16:16:52 -0800

    Render actual lrp placement errors [finishes #87227566]

--- app/components/actual_lrp_list.js
+++ app/components/actual_lrp_list.js
@@ -10,14 +10,29 @@ var ActualLrpList = React.createClass({
     actualLrps: types.array.isRequired
   },
 
-  renderActualLrp({cell_id: cellId, index, since, state}, i) {
+  renderLrpState({state, cellId, placementError}) {
+    state = state && state.toLowerCase();
+    if(placementError) {
+      return (
+        <td className="phm" colSpan='2'><span className="state">{state}: </span>{placementError}</td>
+      );
+    }
+    return [
+      (<td className="phm state" key="0">{state}</td>),
+      (<td className="phm type-ellipsis-1-line" key="1">{cellId}</td>)
+    ];
+  },
+
+  renderActualLrp({cell_id: cellId, index, since, state, placement_error: placementError}, i) {
     var odd = i % 2;
-    var className = cx({'actual-lrp': true, 'bg-dark-1': !odd, 'bg-dark-2': odd, 'bg-error-1': state === 'CRASHED', 'unclaimed': state === 'UNCLAIMED'});
+    var faded = state === 'UNCLAIMED' && !placementError;
+    var crashed = state === 'CRASHED' || placementError;
+
+    var className = cx({'actual-lrp': true, 'bg-dark-1': !odd && !crashed, 'bg-dark-2': odd && !crashed, 'bg-error-1': crashed, 'faded': faded});
     return (
       <tr className={className} key={index}>
         <td className="phm index txt-c">{index}</td>
-        <td className="phm state">{state && state.toLowerCase()}</td>
-        <td className="phm type-ellipsis-1-line">{cellId}</td>
+        {this.renderLrpState({state, cellId, placementError})}
         <td className="phm since txt-r"><Timeago dateTime={new Date(since / 1000000)}/></td>
       </tr>
     );
--- spec/app/components/actual_lrp_list_spec.js
+++ spec/app/components/actual_lrp_list_spec.js
@@ -7,9 +7,9 @@ describe('ActualLrpList', function() {
     actualLrps = [
       Factory.build('actualLrp', {index: 0}),
       Factory.build('actualLrp', {index: 1, state: 'UNCLAIMED'}),
-      Factory.build('actualLrp', {index: 2, state: 'CRASHED'})
-    ]
-    ;
+      Factory.build('actualLrp', {index: 2, state: 'CRASHED'}),
+      Factory.build('actualLrp', {index: 3, state: 'UNCLAIMED', placement_error: 'We crossed the streams'})
+    ];
     subject = React.render(<ActualLrpList {...{actualLrps}}/>, root);
   });
 
@@ -27,9 +27,15 @@ describe('ActualLrpList', function() {
     expect('.actual-lrp:eq(2)').toHaveClass('bg-error-1');
   });
 
+  it('renders lrps with placement errors with errors', function() {
+    expect('.actual-lrp:eq(3)').toHaveClass('bg-error-1');
+    expect('.actual-lrp:eq(3)').not.toHaveClass('faded');
+    expect('.actual-lrp:eq(3)').toContainText('We crossed the streams');
+  });
+
   it('renders unclaimed lrps faded and gray', function() {
-    expect('.actual-lrp:eq(0)').not.toHaveClass('unclaimed');
-    expect('.actual-lrp:eq(1)').toHaveClass('unclaimed');
-    expect('.actual-lrp:eq(2)').not.toHaveClass('unclaimed');
+    expect('.actual-lrp:eq(0)').not.toHaveClass('faded');
+    expect('.actual-lrp:eq(1)').toHaveClass('faded');
+    expect('.actual-lrp:eq(2)').not.toHaveClass('faded');
   });
 });
