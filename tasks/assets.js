80d069112b3ebb67069021d77294079c7c5b7fc8
Charles Hansen <chansen@pivotal.io>
2015-02-26 11:23:36 -0800

    adding gulp autoprefixer and a gulp cleanup task
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- tasks/assets.js
+++ tasks/assets.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
-var nib = require('nib');
+var del = require('del');
 var plugins = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
 function isProduction() {
   return process.env.NODE_ENV === 'production';
@@ -19,6 +20,7 @@ gulp.task('assets-stylesheets', function() {
     .pipe(plugins.plumber())
     .pipe(plugins.if(!isProduction(), plugins.sourcemaps.init()))
     .pipe(plugins.sass())
+    .pipe(plugins.autoprefixer())
     .pipe(plugins.if(!isProduction(), plugins.sourcemaps.write()))
     .pipe(gulp.dest('public'));
 });
@@ -27,4 +29,16 @@ gulp.task('watch-assets', function() {
   gulp.watch('app/stylesheets/**/*.scss', ['assets-stylesheets']);
 });
 
-gulp.task('assets', ['assets-javascript', 'assets-stylesheets']);
+gulp.task('clean-assets-javascript', function(callback) {
+  del(['public/application.js*'], callback);
+});
+
+gulp.task('clean-assets-stylesheets', function(callback) {
+  del(['public/application.css'], callback);
+});
+
+gulp.task('clean-assets', ['clean-assets-javascript', 'clean-assets-stylesheets']);
+
+gulp.task('assets', function() {
+  runSequence('clean-assets', ['assets-javascript', 'assets-stylesheets']);
+});
