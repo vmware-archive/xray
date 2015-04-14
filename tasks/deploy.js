a22661a205568d9d5b369b042d98c64583100e58
Vinson Chuong <vchuong@pivotal.io>
2015-04-14 10:58:07 -0700

    [finishes #91368814] Update gulp deploy to support multiple environments

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,8 +3,25 @@ var runSequence = require('run-sequence');
 var {spawn} = require('child_process');
 
 gulp.task('cf-push', function(callback) {
-  spawn('bash', ['-c', 'cf target -o xray -s xray && cf push'], {stdio: 'inherit', env: process.env})
-    .on('close', callback);
+  var {ENV: environment} = process.env;
+  var command = (
+    environment === 'ketchup' ?
+      `cf api --skip-ssl-validation api.ketchup.cf-app.com
+       cf login -o xray -s xray -u $(awk -F= '/KETCHUP_USERNAME/ {print $2}' .env) -p $(awk -F= '/KETCHUP_PASSWORD/ {print $2}' .env)
+       cf push` :
+    environment === 'pws' ?
+      `cf api api.run.pivotal.io
+       cf login -o pivotal -s pivotal-ui
+       cf push` :
+    null
+  );
+
+  if (environment) {
+    spawn('bash', ['-c', command], {stdio: 'inherit', env: process.env})
+      .on('close', callback);
+  } else {
+    callback('Please set ENV=ketchup or ENV=pws');
+  }
 });
 
 gulp.task('deploy', function(callback) {
