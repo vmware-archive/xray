bc0d73441e336411987fe90c6b29d11e01c45aee
Vinson Chuong <vchuong@pivotal.io>
2015-04-13 18:01:19 -0700

    [#91368332] Basic layout styling for Setup page
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- app/components/setup.js
+++ app/components/setup.js
@@ -10,8 +10,16 @@ var Setup = React.createClass({
       <div className="xray">
         <div className="page">
           <Header className="main-header"/>
-          <section className="main-content">
-            </section>
+          <section className="main-content setup">
+            <aside>
+              <h1 className="title">Explore the Lattice</h1>
+              <p><strong>X-Ray</strong> is an easy to use dashboard for visualizing Lattice clusters. Point X-Ray at your Lattice deployment to view the distribution and status of your containers</p>
+              <p>X-ray needs a working Lattice environment. Read More</p>
+            </aside>
+            <article>
+              Form
+            </article>
+          </section>
           <Footer className="main-footer"/>
         </div>
       </div>
