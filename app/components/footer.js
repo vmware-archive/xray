0e1fb2924dcf1ea4ad188dad6629c321c5b5feb2
Charles Hansen <chansen@pivotal.io>
2015-04-15 16:49:24 -0700

    Use typography classes in footer [#91368324]
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- app/components/footer.js
+++ app/components/footer.js
@@ -8,8 +8,8 @@ var Footer = React.createClass({
     return (
       <footer className={className}>
         <Canvas src={require('../canvas/brand')} width={74} height={17}/>
-        <p>&copy; 2015 Pivotal Software Inc. All rights reserved.</p>
-        <InlineList>
+        <p className="type-xs">&copy; 2015 Pivotal Software Inc. All rights reserved.</p>
+        <InlineList className="type-xs">
           <ListItem><a href="http://pivotal.io/terms-of-use">Terms of Service</a></ListItem>
           <ListItem><a href="http://pivotal.io/privacy-policy">Privacy Policy</a></ListItem>
         </InlineList>
