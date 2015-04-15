f6b48aed35781ca55becc82a58062e8d824773cc
Vinson Chuong <vchuong@pivotal.io>
2015-04-15 11:34:34 -0700

    make a staging environment for xray [finishes #92541516]
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -2,26 +2,25 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var {spawn} = require('child_process');
 
-gulp.task('cf-push', function(callback) {
-  var {ENV: environment} = process.env;
-  var command = (
-    environment === 'ketchup' ?
-      `cf api --skip-ssl-validation api.ketchup.cf-app.com
-       cf login -o xray -s xray -u $(awk -F= '/KETCHUP_USERNAME/ {print $2}' .env) -p $(awk -F= '/KETCHUP_PASSWORD/ {print $2}' .env)
-       cf push` :
-    environment === 'pws' ?
-      `cf api api.run.pivotal.io
-       cf login -o pivotal -s pivotal-ui
-       cf push` :
-    null
-  );
+const COMMANDS = {
+  ketchup: `cf api --skip-ssl-validation api.ketchup.cf-app.com
+            cf login -o xray -s xray -u $(awk -F= '/KETCHUP_USERNAME/ {print $2}' .env) -p $(awk -F= '/KETCHUP_PASSWORD/ {print $2}' .env)
+            cf push`,
+  pws: `cf api api.run.pivotal.io
+        cf login -o pivotal -s pivotal-ui
+        cf push`,
+  staging: `cf api api.run.pivotal.io
+            cf login -o pivotal -s pivotal-ui
+            cf push -f manifest-staging.yml`
+};
 
-  if (environment) {
-    spawn('bash', ['-c', command], {stdio: 'inherit', env: process.env})
-      .on('close', callback);
-  } else {
-    callback('Please set ENV=ketchup or ENV=pws');
+gulp.task('cf-push', function(callback) {
+  var command = COMMANDS[process.env.ENV];
+  if (!command) {
+    return callback('Please set ENV to ketchup, pws, or staging');
   }
+  spawn('bash', ['-c', command], {stdio: 'inherit', env: process.env})
+    .on('close', callback);
 });
 
 gulp.task('deploy', function(callback) {
