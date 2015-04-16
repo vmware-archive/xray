550d1562cddb486beb3493bb476ccf87e486e6f1
Spencer Hurst <pair+shurst@pivotallabs.com>
2015-04-16 13:12:07 -0400

    Add Pivotal UI Grids - Wrap article and aside in md-12 columns

--- app/components/setup.js
+++ app/components/setup.js
@@ -3,6 +3,8 @@ var Header = require('./header');
 var Footer = require('./footer');
 var FormGroup = require('./form_group');
 var Layout = require('../../server/components/layout');
+var {Row} = require('pui-react-grids');
+var {Col} = require('pui-react-grids');
 var {HighlightButton} = require('pui-react-buttons');
 var React = require('react/addons');
 var SetupApi = require('../api/setup_api');
@@ -42,28 +44,37 @@ var Setup = React.createClass({
         <div className="page">
           <Header className="main-header"/>
           <section className="main-content setup">
-            <aside>
-              <h1 className="title em-low">Explore the Lattice</h1>
-              <h2 className="em-low"><span className="em-high">X-Ray</span> is an easy to use dashboard for visualizing Lattice clusters. Point X-Ray at your Lattice deployment to view the distribution and status of your containers</h2>
-              <p className="em-low">X-ray needs a working Lattice environment. <a className="link-text link-inverse" href="http://lattice.cf">Read More</a></p>
-            </aside>
-            <article>
-              <form action="/setup" method="POST" role="form" onSubmit={this.submit}>
-                <div className="form-group">
-                  <h2 className="em-low">What's Your Lattice Receptor URL?</h2>
-                </div>
-                <FormGroup className="receptor-url" onValidate={this.validateReceptorUrl} ref="receptorUrl">
-                  <input autoFocus className="form-control input-lg" name="receptorUrl" placeholder="http://receptor.example.com" value={receptorUrl} onChange={this.change}/>
-                  <HighlightButton type="submit" large>Submit</HighlightButton>
-                </FormGroup>
-                <FormGroup helpBlock="You must accept the terms of service to continue!" onValidate={this.validateTos} ref="ToS">
-                  <label className="checkbox-inline control-label">
-                    <input type="checkbox" name="acceptTos" checked={acceptTos} onChange={this.change}/>
-                    Do you accept our Terms of Service?
-                  </label>
-                </FormGroup>
-              </form>
-            </article>
+            <div className="container">
+              <Row>
+                <Col md={12}>
+                  <aside>
+                    <h1 className="title em-low">Explore the Lattice</h1>
+                    <h2 className="em-low"><span className="em-high">X-Ray</span> is an easy to use dashboard for visualizing Lattice clusters. Point X-Ray at your Lattice deployment to view the distribution and status of your containers</h2>
+                    <p className="em-low">X-ray needs a working Lattice environment. <a className="link-text link-inverse" href="http://lattice.cf">Read More</a></p>
+                  </aside>
+                </Col>
+                <hr className="divider-1 hidden-md hidden-lg" />
+                <Col md={12}>
+                  <article>
+                    <form action="/setup" method="POST" role="form" onSubmit={this.submit}>
+                      <div className="form-group">
+                        <h2 className="em-low">What's Your Lattice Receptor URL?</h2>
+                      </div>
+                      <FormGroup className="receptor-url" onValidate={this.validateReceptorUrl} ref="receptorUrl">
+                        <input autoFocus className="form-control input-lg" name="receptorUrl" placeholder="http://receptor.example.com" value={receptorUrl} onChange={this.change}/>
+                        <HighlightButton type="submit" large>Submit</HighlightButton>
+                      </FormGroup>
+                      <FormGroup helpBlock="You must accept the terms of service to continue!" onValidate={this.validateTos} ref="ToS">
+                        <label className="checkbox-inline control-label">
+                          <input type="checkbox" name="acceptTos" checked={acceptTos} onChange={this.change}/>
+                          Do you accept our Terms of Service?
+                        </label>
+                      </FormGroup>
+                    </form>
+                  </article>
+                </Col>
+              </Row>
+            </div>
           </section>
           <Footer className="main-footer"/>
         </div>
