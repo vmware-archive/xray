1805837be99dedffdae27bd24a88780f3005e076
Charles Hansen <chansen@pivotal.io>
2015-04-20 10:13:43 -0700

    use svg instead of canvas for logos [finishes #92844450]
    Signed-off-by: Ryan Dy <rdy@pivotal.io>

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,5 +1,5 @@
 var React = require('react/addons');
-var Canvas = require('./canvas');
+var Brand = require('../svg/brand');
 var {Flag} = require('pui-react-media');
 var {InlineList, ListItem} = require('pui-react-lists');
 
@@ -8,7 +8,7 @@ var Footer = React.createClass({
     var {className} = this.props;
     return (
       <footer className={className}>
-        <Flag leftImage={<Canvas src={require('../canvas/brand')} width={74} height={17}/>}>
+        <Flag leftImage={<Brand className="brand" width={74} height={17}/>}>
           <InlineList className="type-xs">
             <ListItem>&copy; 2015 Pivotal Software Inc. All rights reserved.
               <div className="hidden-sm hidden-md hidden-lg"><br/></div>
--- app/components/header.js
+++ app/components/header.js
@@ -1,5 +1,5 @@
 var React = require('react/addons');
-var Canvas = require('./canvas');
+var Logo = require('../svg/logo');
 var {Flag} = require('pui-react-media');
 
 var Header = React.createClass({
@@ -7,7 +7,7 @@ var Header = React.createClass({
     var {className, children} = this.props;
     return (
       <header className={className}>
-        <Flag leftImage={<Canvas src={require('../canvas/logo')} className="logo" width={40} height={40}/>} rightImage={children} >
+        <Flag leftImage={<Logo className="logo"/>} rightImage={children}>
           <h1>X-Ray</h1>
         </Flag>
       </header>
