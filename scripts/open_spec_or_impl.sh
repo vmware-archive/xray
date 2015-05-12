27eb6ad034a5a06a74ff98cf706b450a324d3d6f
Ryan Dy <rdy@pivotal.io>
2015-05-11 17:16:34 -0700

    Improve spec swapper for pui-react
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- scripts/open_spec_or_impl.sh
+++ scripts/open_spec_or_impl.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "This script must be called with an argument" >&2
-    exit 1
+if [ -z "$2" ]; then
+  echo "This script must be called with file path and project root" >&2
+  exit 1
 fi
 
-
-echo $1
 if [[ "$1" =~ .*spec\.js ]]; then
-    #in a spec file
-    if [ -d "app" ]; then
-        # source files are in app/, spec files are in spec/app/
-        file_to_open_part1=${1/spec\//\/}
-    else
-       # source files are in src/, spec files are in spec/
-       file_to_open_part1=${1/spec\//src\//}
-    fi
-    file_to_open=${file_to_open_part1/_spec\.js/\.js}
+  #in a spec file
+  if [ -d "src" ]; then
+    # source files are in src/, spec files are in spec/
+    file_to_open_part1=${1/spec\//src\//}
+  else
+    # source files are in root/, spec files are in root/spec/
+    file_to_open_part1=$2${1#$2"/spec"}
+  fi
+  file_to_open=${file_to_open_part1/_spec\.js/\.js}
 else
-    if [ -d "app" ]; then
-        # source files are in app/, spec files are in spec/app/
-        file_to_open_part1=${1/app\//spec\/app\/}
-    else
-       # source files are in src/, spec files are in spec/
-        file_to_open_part1=${1/src\//spec\//}
-    fi
-    file_to_open=${file_to_open_part1/\.js/_spec\.js}
+  if [ -d "src" ]; then
+    # source files are in src/, spec files are in spec/
+    file_to_open_part1=${1/src\//spec\//}
+  else
+    # source files are in root, spec files are in root/spec
+    file_to_open_part1=$2"/spec"${1#$2}
+  fi
+  file_to_open=${file_to_open_part1/\.js/_spec\.js}
 fi
 
 touch $file_to_open
