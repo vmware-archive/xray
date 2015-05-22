7e0de4e6bdcf21bd5c11e4aa04971977ab8c4673
Ryan Dy <rdy@pivotal.io>
2015-05-22 10:14:56 -0700

    fix closure leak problem in blue-green deploy task
    Signed-off-by: Charles Hansen <chansen@pivotal.io>

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -3,28 +3,25 @@ var runSequence = require('run-sequence');
 var {spawn} = require('child_process');
 const DEPLOY_ENVIRONMENTS = require('../config/deploy.json');
 
-gulp.task('blue-green-deploy', (function() {
-  var child;
-  ['SIGINT', 'SIGTERM'].forEach(e => process.on(e, () => {
-    child && child.kill();
-  }));
-  return function(callback) {
-    if (!(process.env.ENV in DEPLOY_ENVIRONMENTS)) {
-      return callback(`Please set ENV to ${Object.keys(DEPLOY_ENVIRONMENTS).join(' or ')}`);
-    }
-    var deployEnv = DEPLOY_ENVIRONMENTS[process.env.ENV];
-    var uppercaseDeployEnv = Object.keys(deployEnv).reduce((memo, key) => (memo[key.toUpperCase()] = deployEnv[key], memo), {});
-    var env = Object.assign(
-      {},
-      process.env,
-      uppercaseDeployEnv
-    );
-    child = spawn('scripts/blue-green-deploy.sh', [], {stdio: 'inherit', env}).on('close', function(...args){
-      child = null;
-      callback(...args);
-    });
-  };
-})());
+gulp.task('blue-green-deploy', function(callback) {
+  if (!(process.env.ENV in DEPLOY_ENVIRONMENTS)) {
+    return callback(`Please set ENV to ${Object.keys(DEPLOY_ENVIRONMENTS).join(' or ')}`);
+  }
+  var deployEnv = DEPLOY_ENVIRONMENTS[process.env.ENV];
+  var uppercaseDeployEnv = Object.keys(deployEnv).reduce((memo, key) => (memo[key.toUpperCase()] = deployEnv[key], memo), {});
+  var env = Object.assign(
+    {},
+    process.env,
+    uppercaseDeployEnv
+  );
+  var child = spawn('scripts/blue-green-deploy.sh', [], {stdio: 'inherit', env}).on('close', function(...args) {
+    child = null;
+    callback(...args);
+  });
+  ['SIGINT', 'SIGTERM'].forEach(e => {
+    process.once(e, () => child && child.kill());
+  });
+});
 
 gulp.task('deploy', function(callback) {
   var env = process.env.NODE_ENV;
