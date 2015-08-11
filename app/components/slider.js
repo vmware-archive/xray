5783ce8437eccde5ac78ca5693b8ca017bc927df
Charles Hansen <chansen@pivotal.io>
2015-08-11 11:39:53 -0700

    Make slider respect now, minor slider tweaks
    Signed-off-by: Tyler Phelan <tphelan@pivotal.io>

--- app/components/slider.js
+++ app/components/slider.js
@@ -1,7 +1,10 @@
+var raf = (typeof document !== 'undefined') ? require('raf') : null;
 var React = require('react');
 var ReactSlider = require('react-slider');
 
 var types = React.PropTypes;
+
+var privates = new WeakMap();
 
 var Slider = React.createClass({
 
@@ -11,10 +14,15 @@ var Slider = React.createClass({
 
   change(value) {
     var {$currentTime} = this.props;
+    var {now} = privates.get(this);
+    if(value === now) {
+      value = 'now';
+    }
     $currentTime.set(value);
   },
 
   getInitialState() {
+    privates.set(this, {now: Date.now()});
     return {
       min: Date.now()
     }
@@ -22,14 +30,22 @@ var Slider = React.createClass({
 
   render() {
     var {$currentTime} = this.props;
-    var now = Date.now();
     var value = $currentTime.get();
-
     var {min} = this.state;
+    var now = Date.now();
+    min = Math.min(min, now - 5 * 60 * 1000);
+
+    var rafHandle;
 
     if(value === 'now') {
-      value = Date.now();
+      value = now;
+    } else {
+      var {rafHandle} = privates.get(this);
+      raf.cancel(rafHandle);
+      rafHandle = raf(() => {this.isMounted() && this.forceUpdate()});
     }
+
+    privates.set(this, {now, rafHandle});
 
     return (
       <ReactSlider max={now} min={min} onChange={this.change} value={value}/>
